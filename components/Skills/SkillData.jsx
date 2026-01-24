2b6bb8b22f472873d10ad69111e3835a86565c7f
Nadun Daluwatta <nadundaluwatta26@gmail.com>
2026-01-24 22:32:14 +0530

    fix: update SkillData.jsx icon import and usage

--- components/Skills/SkillData.jsx
+++ components/Skills/SkillData.jsx
@@ -1,35 +1,51 @@
-import { FaJsSquare, FaReact, FaNodeJs, FaDatabase, FaDocker, FaGitAlt, FaCss3Alt } from "react-icons/fa";
-import { SiTailwindcss, SiExpress, SiPython, SiJava, SiMysql, SiFirebase, SiFigma, SiMarkdown, SiNextdotjs } from "react-icons/si";
+import {
+  FaJsSquare,
+  FaReact,
+  FaNodeJs,
+  FaDatabase,
+  FaDocker,
+  FaGitAlt,
+  FaCss3Alt
+} from "react-icons/fa";
+
+import {
+  SiTailwindcss,
+  SiPython,
+  SiMysql,
+  SiFirebase,
+  SiFigma,
+  SiMarkdown,
+  SiNextdotjs,
+  SiFlutter
+} from "react-icons/si";
+
 import { VscVscode } from "react-icons/vsc";
 import { AiFillHtml5 } from "react-icons/ai";
 import { FaJava } from "react-icons/fa6";
-import { SiFlutter } from "react-icons/si";
 
 export const skillData = {
-    mainData: {
-        title: "Skills",
-        description: "A showcase of my professional technical abilities."
-    },
-    technical: [
-        { name: 'JavaScript', icon: FaJsSquare },
-        { name: 'React.js', icon: FaReact },
-        { name: 'Node.js', icon: FaNodeJs },
-        { name: 'CSS3', icon: FaCss3Alt },
-        { name: 'HTML5', icon: AiFillHtml5 },
-        { name: 'Java', icon: FaJava },
-        { name: 'Tailwind CSS', icon: SiTailwindcss  },
-        { name: 'Flutter', icon: SiFlutter },
-        { name: 'MongoDB', icon: FaDatabase },
-        { name: 'Python', icon: SiPython },
-        { name: 'MySQL', icon: SiMysql },
-        { name: 'Git & GitHub', icon: FaGitAlt },
-        { name: 'Docker', icon: FaDocker },
-        { name: 'Firebase', icon: SiFirebase },
-        { name: 'Figma', icon: SiFigma },
-        { name: 'Next.js', icon: SiNextdotjs },
-        { name: 'Packet Tracer', icon: VscVscode },
-        { name: 'GNS3', icon: SiMarkdown }
-        
-        
-    ]
+  mainData: {
+    title: "Skills",
+    description: "A showcase of my professional technical abilities."
+  },
+  technical: [
+    { name: "JavaScript", icon: FaJsSquare },
+    { name: "React.js", icon: FaReact },
+    { name: "Node.js", icon: FaNodeJs },
+    { name: "CSS3", icon: FaCss3Alt },
+    { name: "HTML5", icon: AiFillHtml5 },
+    { name: "Java", icon: FaJava },
+    { name: "Tailwind CSS", icon: SiTailwindcss },
+    { name: "Flutter", icon: SiFlutter },
+    { name: "MongoDB", icon: FaDatabase },
+    { name: "Python", icon: SiPython },
+    { name: "MySQL", icon: SiMysql },
+    { name: "Git & GitHub", icon: FaGitAlt },
+    { name: "Docker", icon: FaDocker },
+    { name: "Firebase", icon: SiFirebase },
+    { name: "Figma", icon: SiFigma },
+    { name: "Next.js", icon: SiNextdotjs },
+    { name: "Packet Tracer", icon: VscVscode },
+    { name: "GNS3", icon: SiMarkdown }
+  ]
 };
